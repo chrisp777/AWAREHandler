27d28969deffbbd2eb46fa830294fd0cbdd18f16
Chris <Chrispdavenport@googlemail.com>
2014-10-23 17:58:44 +0100

    Removed pythonPath and renamed DirChecker to dirChecker, also changed method to use pyinotify.

---
+++ AWAREHandler/dirChecker.py
@@ -0,0 +1,20 @@
+'''
+Created on 23 Oct 2014
+
+@author: Chris
+'''
+import pyinotify
+class TrackModifications(pyinotify.ProcessEvent):
+    def process_IN_MODIFY(selfself, Event):
+        print 'IN_MODIFY'
+
+# Instanciate a new WatchManager (will be used to store watches).
+wm = pyinotify.WatchManager()
+# Associate this WatchManager with a Notifier (will be used to report and
+# process events).
+handler = Empty(TrackModifications())
+notifier = pyinotify.Notifier(wm)
+# Add a new watch on /test for ALL_EVENTS.
+wm.add_watch('/test', pyinotify.ALL_EVENTS)
+# Loop forever and handle events.
+notifier.loop()
