dac20cb13895a82a015f14549bbbffcdecf3a583
Chris <Chrispdavenport@googlemail.com>
2014-10-23 23:02:01 +0100

    d

--- AWAREHandler/dirChecker.py
+++ AWAREHandler/dirChecker.py
@@ -5,21 +5,35 @@ Created on 23 Oct 2014
 '''
 import pyinotify
 
-class TrackModifications(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(selfself, Event):
-        print 'IN_MODIFY'
-        
-class Empty(pyinotify.processEvent):
-    self._mesg = msg
-    print msg
+class MyEventHandler(pyinotify.ProcessEvent):
+    def my_init(self, file_object=sys.stdout):
+        """
+        This is your constructor it is automatically called from ProcessEvent.__init__(),
+        And extra arguments passed to __init__() would be delegated automatically to 
+        my_init().
+        """
+        self._file_object = file_object
 
-# Instanciate a new WatchManager (will be used to store watches).
-wm = pyinotify.WatchManager()
-# Associate this WatchManager with a Notifier (will be used to report and
-# process events).
-handler = Empty(TrackModifications())
-notifier = pyinotify.Notifier(wm)
-# Add a new watch on /test for ALL_EVENTS.
-wm.add_watch('/test', pyinotify.ALL_EVENTS)
-# Loop forever and handle events.
-notifier.loop()
+    def process_IN_DELETE(self, event):
+        """
+        This method processes a specific type of event: IN_DELETE. event
+        is an instance of Event.
+        """
+        self._file_object.write('deleting: %s\n' % event.pathname)
+
+    def process_IN_CLOSE(self, event):
+        """
+        This method is called on these events: IN_CLOSE_WRITE and
+        IN_CLOSE_NOWRITE.
+        """
+        self._file_object.write('closing: %s\n' % event.pathname)
+
+    def process_default(self, event):
+        """
+        Eventually, this method is called for all others types of events.
+        This method can be useful when an action fits all events.
+        """
+        self._file_object.write('default processing\n')
+
+# A way to instantiate this class could be:
+p = MyEventHandler(file('/test', 'w'))
